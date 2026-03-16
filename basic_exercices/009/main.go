9b47b8dba5aa0fb7655235e05ef3a194ed9118ac
jiawei-hong <zxc37984@gmail.com>
2026-03-17 03:10:02 +0800

    feat: basic-exercise 009

--- basic_exercices/009/main.go
+++ basic_exercices/009/main.go
@@ -9,5 +9,8 @@ func main () {
 	// initialized array of 10 int values [0..9]
 	var arr = [10]int{0,1,2,3,4,5,6,7,8,9}
 	// Here goes your code
+	for _, v := range arr {
+		fmt.Println(v + 2)
+}
 	
 }
