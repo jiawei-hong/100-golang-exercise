d7c7224b9786a3091935fef9ae541229c41edbef
jiawei-hong <zxc37984@gmail.com>
2026-03-17 03:27:56 +0800

    feat: basic-exercise 015

--- basic_exercices/015/main.go
+++ basic_exercices/015/main.go
@@ -1,12 +1,19 @@
 // Exercise: User input
 // Get a number from the console and check if it's odd
-// You can create another function or do it everything in the main func :) 
+// You can create another function or do it everything in the main func :)
 
 package main
 
 import "fmt"
 
-func main () {
+func main() {
 	// Here goes your code
-	fmt.Printf("...")
+	var number int
+	fmt.Scanln(&number)
+
+	if number%2 == 0 {
+		fmt.Println("is even")
+	} else {
+		fmt.Println("is odd")
+	}
 }
